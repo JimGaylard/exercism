934757538d75e60d2945913dc04e4fc92f6e3e52
Jim Gaylard <jim.gaylard@rea-group.com>
2016-08-14 16:26:49 +1000

    working grade_school solution

--- haskell/grade-school/src/School.hs
+++ haskell/grade-school/src/School.hs
@@ -1,12 +1,25 @@
-module School (School, add, empty, grade, sorted) where
+module School (School(..), add, empty, grade, sorted) where
+
+import qualified Data.Map as M
+import Data.List (sort)
 
 -- The task is to create the data type `School` and
 -- implement the functions below.
 
-add = undefined
+type School = M.Map Grade [Student]
+type Grade = Int
+type Student = String
 
-empty = undefined
+add :: Grade -> Student -> School -> School
+add g s = M.insertWith (++) g [s]
 
-grade = undefined
+empty :: School
+empty = M.empty
 
-sorted = undefined
+grade :: Grade -> School -> [Student]
+grade = M.findWithDefault []
+
+sorted :: School -> [(Grade, [Student])]
+sorted s = map f s' where
+  f (g, ss) = (g, sort ss)
+  s' = M.toAscList s
