b65b47c0bfe4d648ed2305e9c378d7c979bd96de
Jim Gaylard <jim.gaylard@rea-group.com>
2016-08-18 20:33:18 +1000

    better reverse function

--- haskell/simple-linked-list/src/LinkedList.hs
+++ haskell/simple-linked-list/src/LinkedList.hs
@@ -13,7 +13,11 @@ module LinkedList
 -- The task is to create the data type `LinkedList`
 -- and implement the functions below.
 
-data LinkedList a = Nil | Cons a (LinkedList a)
+data LinkedList a = Nil | Cons a (LinkedList a) deriving (Show)
+
+instance Foldable (LinkedList) where
+  foldr f z Nil = z
+  foldr f b (Cons x xs) = foldr f (f x b) xs
 
 datum :: LinkedList a -> a
 datum (Cons x xs) = x
@@ -36,7 +40,7 @@ nil :: LinkedList a
 nil = Nil
 
 reverseLinkedList :: LinkedList a -> LinkedList a
-reverseLinkedList x = fromList $ reverse (toList x)
+reverseLinkedList = foldr Cons Nil
 
 toList :: LinkedList a -> [a]
 toList Nil = []
