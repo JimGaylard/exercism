b624d363f9ac85e2cb9d11700f91f00329516c08
Jim Gaylard <jim.gaylard@rea-group.com>
2016-08-21 17:03:21 +1000

    working solution to etl

--- haskell/etl/src/ETL.hs
+++ haskell/etl/src/ETL.hs
@@ -1,6 +1,9 @@
 module ETL (transform) where
 
-import Data.Map (Map)
+import Data.Map (Map, fromList, toList)
+import Data.Char (toLower)
 
 transform :: Map a String -> Map Char a
-transform = undefined
+transform x = fromList $ scoresIn $ toList x where
+  scoresIn = foldr ((++) . scoreIn) []
+  scoreIn (i, ys) = zip (map toLower ys) (repeat i)
