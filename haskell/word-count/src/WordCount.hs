609d26a6f30956aa973791fb800eeaaa34436f64
Jim Gaylard <jim.gaylard@rea-group.com>
2016-08-21 16:31:38 +1000

    working solution for word-count

--- haskell/word-count/src/WordCount.hs
+++ haskell/word-count/src/WordCount.hs
@@ -1,3 +1,17 @@
 module WordCount (wordCount) where
 
-wordCount = undefined
+import Data.Char (toLower)
+
+import qualified Data.Map as M
+
+wordCount :: String -> M.Map String Int
+wordCount s = M.fromListWith (+) $ zip (realWords s) (repeat 1)
+
+realWords :: String -> [String]
+realWords s = words $ map toLower (replace s)
+
+replace :: String -> String
+replace = map (\c -> if isOk c then c else ' ')
+
+isOk :: Char -> Bool
+isOk c = c `notElem` "_#&@$%^!?:'\",."
