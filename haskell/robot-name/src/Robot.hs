bd5b48839b261e066e2193194a1e99b63db4d836
Jim Gaylard <jim.gaylard@rea-group.com>
2016-08-29 22:05:13 +1000

    resetName still not passing

--- haskell/robot-name/src/Robot.hs
+++ haskell/robot-name/src/Robot.hs
@@ -3,17 +3,25 @@ module Robot (Robot, mkRobot, resetName, robotName) where
 import qualified System.Random as R
 import Data.Char (toUpper, intToDigit)
 
-data Robot = Robot String deriving Show
+data Robot = Robot { name :: String } deriving Show
 
 mkRobot :: IO Robot
 mkRobot = do
+  name' <- mkName
+  return $ Robot name'
+
+mkName :: IO String
+mkName = do
   gen <- R.newStdGen
   let letters = take 2 $ map toUpper $ R.randomRs ('a', 'z') gen :: String
       numbers = take 3 $ R.randomRs (0, 9) gen :: [Int] in
-        return $ Robot (letters ++ map intToDigit numbers)
+        return $ letters ++ map intToDigit numbers
 
 resetName :: Robot -> IO ()
-resetName = undefined
+resetName x = do
+  name' <- mkName
+  pure x { name = name' }
+  return ()
 
 robotName :: Robot -> IO String
-robotName (Robot s) = return s
+robotName r = return $ name r
