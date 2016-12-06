08584fb5670b2f3c643ee4ce4be8fec185aa8c17
Jim Gaylard <jim@jimgaylard.com>
2016-12-06 10:07:32 +0000

    use multiset for word-count

--- haskell/word-count/src/WordCount.hs
+++ haskell/word-count/src/WordCount.hs
@@ -1,12 +1,13 @@
 module WordCount (wordCount) where
 
 import Data.Char (toLower, isAlphaNum)
+import qualified Data.MultiSet as MultiSet
 import Data.List.Split (wordsBy)
 
 import qualified Data.Map as M
 
 wordCount :: String -> M.Map String Int
-wordCount s = M.fromListWith (+) $ zip (realWords s) (repeat 1)
+wordCount s = MultiSet.toMap $ MultiSet.fromList (realWords s)
 
 realWords :: String -> [String]
 realWords s = wordsBy (not . isAlphaNum) $ map toLower s
