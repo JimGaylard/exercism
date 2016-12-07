ded61e5a65c1f18797f6fcb97b3936c545ea41ac
Jim Gaylard <jim@jimgaylard.com>
2016-12-07 22:32:41 +0000

    basic raindrops solution

--- haskell/raindrops/src/Raindrops.hs
+++ haskell/raindrops/src/Raindrops.hs
@@ -1,3 +1,13 @@
 module Raindrops (convert) where
 
-convert = undefined
+convert :: (Integral a, Show a) => a -> String
+convert n
+  | factorOf 3 && factorOf 5 && factorOf 7 = "PlingPlangPlong"
+  | factorOf 3 && factorOf 5 = "PlingPlang"
+  | factorOf 3 && factorOf 7 = "PlingPlong"
+  | factorOf 5 && factorOf 7 = "PlangPlong"
+  | factorOf 3 = "Pling"
+  | factorOf 5 = "Plang"
+  | factorOf 7 = "Plong"
+  | otherwise = show n where
+    factorOf x = mod n x == 0
