fea31994bb66b254beefcfd9f3d18b76077aef90
Jim Gaylard <jim@jimgaylard.com>
2016-12-08 05:39:42 +0000

    fetched triangle

---
+++ haskell/triangle/src/Triangle.hs
@@ -0,0 +1,9 @@
+module Triangle (TriangleType(..), triangleType) where
+
+data TriangleType = Equilateral
+                  | Isosceles
+                  | Scalene
+                  | Illegal
+                  deriving (Eq, Show)
+
+triangleType = undefined
