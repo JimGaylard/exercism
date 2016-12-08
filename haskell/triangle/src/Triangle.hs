9193e4865349e49b1bf34857a616547d650041c6
Jim Gaylard <jim@jimgaylard.com>
2016-12-08 08:00:23 +0000

    solution for triangle

--- haskell/triangle/src/Triangle.hs
+++ haskell/triangle/src/Triangle.hs
@@ -1,9 +1,23 @@
 module Triangle (TriangleType(..), triangleType) where
 
+import Data.MultiSet (MultiSet)
+import qualified Data.MultiSet as M
+
 data TriangleType = Equilateral
                   | Isosceles
                   | Scalene
                   | Illegal
                   deriving (Eq, Show)
 
-triangleType = undefined
+triangleType :: (Num a, Ord a) => a -> a -> a -> TriangleType
+triangleType a b c
+  | invalid a b c    = Illegal
+  | commonSides == 1 = Equilateral
+  | commonSides == 2 = Isosceles
+  | commonSides == 3 = Scalene
+  | otherwise        = Illegal where
+    commonSides = M.distinctSize $ M.fromList [a, b, c]
+    invalid x y z =
+         (x + y <= z)
+      || (x + z <= y)
+      || (y + z <= x)
