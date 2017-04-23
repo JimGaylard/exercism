3a2a0b8c44742f0f649b472484b9acdbbb33e0a7
Jim Gaylard <jim@jimgaylard.com>
2017-04-23 16:04:30 +1000

    cleanup

--- haskell/sum-of-multiples/SumOfMultiples.hs
+++ haskell/sum-of-multiples/SumOfMultiples.hs
@@ -4,8 +4,7 @@ import Data.List (nub)
 
 sumOfMultiples :: Integral a => [a] -> a -> a
 sumOfMultiples xs limit =
-  sum $ nub $ multiple <$> nats <*> xs
+  sum $ nub $ multiple <$> [1..limit - 1] <*> xs
         where multiple x y = if x `mod` y == 0
                                 then x
                                 else 0
-              nats = [1..limit - 1]
