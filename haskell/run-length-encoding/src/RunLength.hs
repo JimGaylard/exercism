0ba8249c7a48c5bcb3942a05edb4e15457aa0955
Jim Gaylard <jim@jimgaylard.com>
2017-06-01 22:18:44 +1000

    solution for run-length-encoding

--- haskell/run-length-encoding/src/RunLength.hs
+++ haskell/run-length-encoding/src/RunLength.hs
@@ -1,7 +1,24 @@
 module RunLength (decode, encode) where
 
+import Data.Char (isDigit)
+
+
 decode :: String -> String
-decode encodedText = error "You need to implement this function."
+decode [] = ""
+decode xs = loop xs []  where
+  loop :: String -> String -> String
+  loop [] _ = []
+  loop (y:ys) ns = if isDigit y then loop ys (y : ns)
+                                else printNums ns y ++ loop ys []
+  printNums [] char = [char]
+  printNums ns char = replicate (read $ reverse ns :: Int) char
 
 encode :: String -> String
-encode text = error "You need to implement this function."
+encode [] = ""
+encode (x:xs) = loop xs x 1 where
+  loop :: String -> Char -> Int -> String
+  loop [] y acc = numberFor acc ++ [y]
+  loop (z:zs) y acc = if z == y then loop zs y (acc +1)
+                                else numberFor acc ++ y : loop zs z 1
+  numberFor n = if n == 1 then ""
+                          else show n
