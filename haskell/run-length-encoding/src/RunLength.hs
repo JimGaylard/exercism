374affb2b6ee04f2051491e49c6a9b51df49e6d6
Jim Gaylard <jim@jimgaylard.com>
2017-06-05 10:50:07 +1000

    whitespace

--- haskell/run-length-encoding/src/RunLength.hs
+++ haskell/run-length-encoding/src/RunLength.hs
@@ -9,9 +9,11 @@ decode xs = loop xs []  where
   loop :: String -> String -> String
   loop [] _ = []
   loop (y:ys) ns = if isDigit y then loop ys (y : ns)
-                                else printNums ns y ++ loop ys []
+                                else printNums ns y
+                                  ++ loop ys []
   printNums [] char = [char]
-  printNums ns char = replicate (read $ reverse ns :: Int) char
+  printNums ns char =
+    replicate (read $ reverse ns :: Int) char
 
 encode :: String -> String
 encode [] = ""
@@ -19,6 +21,8 @@ encode (x:xs) = loop xs x 1 where
   loop :: String -> Char -> Int -> String
   loop [] y acc = numberFor acc ++ [y]
   loop (z:zs) y acc = if z == y then loop zs y (acc +1)
-                                else numberFor acc ++ y : loop zs z 1
+                                else numberFor acc
+                                  ++ y
+                                  : loop zs z 1
   numberFor n = if n == 1 then ""
                           else show n
