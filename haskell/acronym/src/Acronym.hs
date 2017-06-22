647ff573445bb4bfddc7954d739ca79e2b69e3d2
Jim Gaylard <jim@jimgaylard.com>
2017-06-22 20:45:17 +1000

    playing with parser combinators

--- haskell/acronym/src/Acronym.hs
+++ haskell/acronym/src/Acronym.hs
@@ -1,6 +1,41 @@
 module Acronym (abbreviate) where
 
-import Data.Char (toUpper)
+import Control.Applicative ((<|>))
+import Data.Char (toUpper, isAlpha, isAsciiLower, isAsciiUpper)
+import Text.ParserCombinators.ReadP (ReadP, eof, many, option, satisfy)
 
 abbreviate :: String -> String
 abbreviate xs = toUpper . head <$> words xs
+
+acroParser :: ReadP [String]
+acroParser = many $ do
+  word <- normalWord
+  _ <- whiteSpace
+  return word
+
+myWord :: ReadP [String]
+myWord = camelWord <|> acronymWord
+
+normalWord :: ReadP String
+normalWord = do
+  first <- letter
+  rest <- many $ satisfy isAsciiLower
+  return $ first : rest
+
+camelWord :: ReadP [String]
+camelWord = do
+  first <- letter
+  lower <- many $ satisfy isAsciiLower
+  upper <- satisfy isAsciiUpper
+  rest <- many $ satisfy isAsciiLower
+  return (first : lower) ++ (upper : rest)
+
+acronymWord :: ReadP String
+acronymWord = undefined
+
+letter :: ReadP Char
+letter =
+  satisfy isAsciiUpper <|> satisfy isAsciiLower
+
+whiteSpace :: ReadP Char
+whiteSpace = satisfy (\c -> any (\ch -> c == ch) " \n\r")
